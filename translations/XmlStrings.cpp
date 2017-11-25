ae76470ea97e2f5eb03df003a12b122f670ecb9b
Jim Evins <evins@snaught.com>
2017-11-25 12:08:06 -0500

    Use modern cmake patterns to build translations/XmlStrings.

--- translations/XmlStrings.cpp
+++ translations/XmlStrings.cpp
@@ -18,7 +18,7 @@
  *  along with gLabels-qt.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <QApplication>
+#include <QCoreApplication>
 #include <QStringList>
 #include <QFile>
 #include <QDomDocument>
