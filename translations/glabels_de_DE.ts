65a33e5c4142b68bd61d11f7cd7affc7e8ffff7d
Mario Blättermann <mario.blaettermann@gmail.com>
2018-06-17 14:55:29 +0200

    Fixed XML structure in German translation

--- translations/glabels_de_DE.ts
+++ translations/glabels_de_DE.ts
@@ -1,3 +1,5 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
 <!--
  ! This file is derived from the previous German translation in Gettext.
  ! Copyright (C) 2001-2018 Jim Evins <evins@snaught.com>
@@ -6,8 +8,6 @@
  ! Christian Neumair <christian-neumair@web.de>, 2002.
  ! Mario Blättermann <mario.blaettermann@gmail.com>, 2008-2018.
   -->
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
 <TS version="2.1" language="de_DE" sourcelanguage="en_US">
 <context>
     <name>AboutDialog</name>
