fd0c56843da0df678cfada1ae1aec5de91186164
hochwasser <hochwasser@windbuechse.samba-tng.org>
2018-11-07 09:28:34 +0100

    include inet.h is not needed anymore

--- model/XmlLabelParser_0_4.cpp
+++ model/XmlLabelParser_0_4.cpp
@@ -17,7 +17,6 @@
  *  You should have received a copy of the GNU General Public License
  *  along with gLabels-qt.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include <arpa/inet.h>
 #include "XmlLabelParser_0_4.h"
 
 #include "Model.h"
